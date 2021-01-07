501db8be046cbf0262b2ea503a23799b2e0e3691
edwin.aditya <edwin.aditya@bukalapak.com>
2021-01-07 23:26:07 +0700

    remove type

--- dumps/Dump20210107.sql
+++ dumps/Dump20210107.sql
@@ -31,7 +31,7 @@ CREATE TABLE `water_heights` (
   `updated_at` datetime DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
